fa69c8b0d74bc162a73407e5e5ebd95323c7f419
Katrik <karthikpai2002@gmail.com>
2024-12-30 01:17:13 +0530

    Integer data types

--- Python_Essentials/Module_1/python_data_types.py
+++ Python_Essentials/Module_1/python_data_types.py
@@ -9,3 +9,20 @@ Common data Types in Python
 # Use single quotes when there is double quotes in the string
 # Use double quotes when there is single quotes in the string
 # Use triple quotes when there is both single or double quotes in the string
+
+# F-Strings
+# Used to use variables within the string
+# Example: 
+name = "John"
+s1 =f"{name} is a boy"
+print(s1)
+
+# Built in function to check the variable data type is type()
+# Example 
+print(type(name))
+
+# Type of the data can be changed depending on the operation being performed 
+# on them
+result = 3/2 # both the operands are integers 
+print(type(result)) # the opeartion results in float
+
